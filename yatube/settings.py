9007d714e7d68c354f3e367b81777eafa5154fa7
Ягунов Денис <spb.jagunov.denis@yandex.ru>
2021-07-19 09:10:00 +0300

    удали django debug toolbar

--- yatube/settings.py
+++ yatube/settings.py
@@ -46,7 +46,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'sorl.thumbnail',
-    "debug_toolbar",
 ]
 
 MIDDLEWARE = [
@@ -57,7 +56,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = 'yatube.urls'
--- yatube/urls.py
+++ yatube/urls.py
@@ -31,13 +31,3 @@ urlpatterns = [
     path('about/', include('about.urls', namespace='about')),
     path('admin/', admin.site.urls),
 ]
-
-if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns += (path("__debug__/", include(debug_toolbar.urls)),)
-    urlpatterns += static(
-        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
-    )
-    urlpatterns += static(
-        settings.STATIC_URL, document_root=settings.STATIC_ROOT
-    )
